82f45b4632b81ee9212a58d9ec95a1f39412e9cf
airjp73 <aaron.pettengill.violin@gmail.com>
2018-02-11 13:49:21 -0500

    minor chagnes to store

--- store/index.js
+++ store/index.js
@@ -3,6 +3,7 @@ import { combineReducers, createStore } from 'redux'
 import todos from './todos/reducers'
 
 const reducers  = combineReducers({todos})
-const store     = createStore(reducers)
 
-export default store
+export const initStore = (initialState = {}) => {
+  return createStore(reducers, initialState)
+}
